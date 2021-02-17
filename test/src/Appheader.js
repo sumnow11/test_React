7709da0d55ceabb6783a9ac69a8be97958d69756
streaming2304 <615021000836@mail.rmutk.ac.th>
2021-02-17 12:19:06 +0700

    Lap

---
+++ test/src/Appheader.js
@@ -0,0 +1,12 @@
+function Appheader() {
+    
+    return (
+       
+          <header className="App-header">
+              <img className ="header-logo" src ="p\87181961_3261780597169375_3695102167389044736_o.jpg"/>
+    
+           
+          </header>
+          );
+          }
+    export default Appheader;
---
+++ test/src/items.js
@@ -0,0 +1,27 @@
+const items =[
+    {
+      title :"man",
+      thumbnaiUrl :"/p/87181961_3261780597169375_3695102167389044736_o.jpg"
+      
+    
+    },
+    {
+      title :"gril",
+      thumbnaiUrl :"/p/CeIXDtoUMAEEelh.jpg"
+      
+    
+    },
+    {
+      title :"boy",
+      thumbnaiUrl :"/p/sample-50048a55841ae51d1e4f6b1760c33f4e.jpg"
+      
+    
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/p/58557c7e454141590dee4d4c.png"
+      
+    
+    }
+  ];
+  export default items;
